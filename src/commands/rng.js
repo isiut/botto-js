7300916ddaf2cf023388e1169b557d3f989bc58d
Illia Siutkin <illiasiutkin07@gmail.com>
2022-10-15 10:39:44 -0500

    add options to rng command

--- src/commands/rng.js
+++ src/commands/rng.js
@@ -3,9 +3,21 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('rng')
-    .setDescription('Replies with a random number'),
+    .setDescription('Replies with a random number')
+    .addNumberOption(option => 
+        option
+            .setName('max')
+            .setDescription('The maximum random number')
+            .setRequired(true)
+            .addChoices(
+                { name: '0-10', value: 10 },
+                { name: '0-100', value: 100 },
+                { name: '0-1000', value: 1000 },
+            )
+    ),
     async execute(interaction) {
-        let randomNumber = String(Math.round(Math.random() * 100));
+        const multiplier = interaction.options.getNumber('max');
+        let randomNumber = String(Math.round(Math.random() * multiplier));
 
         await interaction.reply(randomNumber);
     }
