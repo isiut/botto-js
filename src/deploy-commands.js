96598083bcca686135c9993bb32e4d3747078785
Illia Siutkin <illiasiutkin07@gmail.com>
2022-10-14 21:13:22 -0500

    add slash command deploy script

---
+++ src/deploy-commands.js
@@ -0,0 +1,16 @@
+// Only needs to be run once each time a new command is added or edited
+
+const { REST, SlashCommandBuilder, Routes } = require('discord.js');
+const { clientId, guildId, token } = require('./config.json');
+
+const commands = [
+    new SlashCommandBuilder().setName('ping').setDescription('Replies with the latency'),
+    new SlashCommandBuilder().setName('info').setDescription('Replies with information about the bot')
+]
+    .map(command => command.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+    .then((data) => console.log(`Successfully registered ${data.length} application commands.`))
+    .catch(console.error);
