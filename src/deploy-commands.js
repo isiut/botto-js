5fe6b939c314dfef586e9dc657f27c84450c74ba
Illia Siutkin <illiasiutkin07@gmail.com>
2022-10-15 10:41:22 -0500

    format for consistency

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,22 +1,29 @@
 // Only needs to be run once each time a new command is added or edited
 
-const fs = require('node:fs');
-const path = require('node:path');
-const { REST, SlashCommandBuilder, Routes } = require('discord.js');
-const { clientId, guildId, token } = require('./config.json');
+const fs = require("node:fs");
+const path = require("node:path");
+const { REST, SlashCommandBuilder, Routes } = require("discord.js");
+const { clientId, guildId, token } = require("./config.json");
 
 const commands = [];
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    commands.push(command.data.toJSON());
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: '10' }).setToken(token);
+const rest = new REST({ version: "10" }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-    .then((data) => console.log(`Successfully registered ${data.length} application command(s).`))
-    .catch(console.error);
+rest
+  .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+  .then((data) =>
+    console.log(
+      `Successfully registered ${data.length} application command(s).`
+    )
+  )
+  .catch(console.error);
