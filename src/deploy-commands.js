246f348c2edfc1c85691da4daca643b488e77e84
Illia Siutkin <illiasiutkin07@gmail.com>
2022-10-14 21:42:46 -0500

    create command handling

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,16 +1,22 @@
 // Only needs to be run once each time a new command is added or edited
 
+const fs = require('node:fs');
+const path = require('node:path');
 const { REST, SlashCommandBuilder, Routes } = require('discord.js');
 const { clientId, guildId, token } = require('./config.json');
 
-const commands = [
-    new SlashCommandBuilder().setName('ping').setDescription('Replies with the latency'),
-    new SlashCommandBuilder().setName('info').setDescription('Replies with information about the bot')
-]
-    .map(command => command.toJSON());
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    commands.push(command.data.toJSON());
+}
 
 const rest = new REST({ version: '10' }).setToken(token);
 
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-    .then((data) => console.log(`Successfully registered ${data.length} application commands.`))
+    .then((data) => console.log(`Successfully registered ${data.length} application command(s).`))
     .catch(console.error);
