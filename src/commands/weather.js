daf3ff77bf156748943d5b8ab388194834fe4ed9
Illia Siutkin <illiasiutkin07@gmail.com>
2022-11-06 12:32:06 -0600

    add weather command

---
+++ src/commands/weather.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require("discord.js");
+const weather = require("weather-js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("weather")
+        .setDescription("Returns the weather for a city")
+        .addStringOption((option) =>
+            option
+                .setName("city")
+                .setDescription("The city to display the weather for")
+                .setRequired(true)
+        ),
+    async execute(interaction) {
+        const city = interaction.options.getString("city");
+        let noLocationFound = false;
+        let location;
+
+        weather.find({ search: city }, async function (err, result) {
+            if (err) {
+                console.log(err);
+                noLocationFound = true;
+            }
+
+            location = result[0].location.name;
+            conditions = result[0].current.skytext;
+            temperature = result[0].current.temperature;
+
+            await interaction.reply(
+                `Weather for ${location}:\n- Conditions: ${conditions}\n- Temperature: ${temperature}°F`
+            );
+        });
+    },
+};
