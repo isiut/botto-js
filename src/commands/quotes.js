35de716265c02bd848d94a4aff5725a924cc10fb
Illia Siutkin <illiasiutkin07@gmail.com>
2022-11-07 19:44:58 -0600

    add random quote command

---
+++ src/commands/quotes.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require("discord.js");
+const qt = require("./misc/quotes.json");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("quote")
+        .setDescription("Returns a random quote"),
+    async execute(interaction) {
+        const rand = Math.round(Math.random() * 101);
+
+        const quote = qt.quotes[rand].quote;
+        const author = qt.quotes[rand].author;
+
+        await interaction.reply(`${quote}\n\n- ${author}`);
+    },
+};
