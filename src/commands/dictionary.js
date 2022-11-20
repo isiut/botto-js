93b72ca9f06c7b7778fbd4bd122a59c0dd29d9ef
Illia Siutkin <illiasiutkin07@gmail.com>
2022-11-20 00:01:21 -0600

    add dictionary lookup command

---
+++ src/commands/dictionary.js
@@ -0,0 +1,35 @@
+// Issue: Replies with duplicated definitions
+// when command is run more than once
+
+const { SlashCommandBuilder } = require("discord.js");
+const wordnet = require("wordnet");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("dictionary")
+        .setDescription("looks a word up in the dictionary")
+        .addStringOption((option) =>
+            option
+                .setName("word")
+                .setDescription("the word to look up")
+                .setRequired(true)
+        ),
+    async execute(interaction) {
+        const word = interaction.options.getString("word");
+        let msg = `${word}:\n`;
+
+        await wordnet.init();
+        wordnet
+            .lookup(word, (skipPointers = true))
+            .then(async (definitions) => {
+                definitions.forEach(async (def) => {
+                    msg += `- ${def.glossary}\n`;
+                });
+
+                await interaction.reply(msg);
+            })
+            .catch(async (e) => {
+                await interaction.reply("Word not found");
+            });
+    },
+};
