a4c84f0d0aced498d0b370d80ba113a82f5c4db7
Illia Siutkin <illiasiutkin07@gmail.com>
2022-11-19 18:40:17 -0600

    change commands to register globally

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -19,7 +19,7 @@ for (const file of commandFiles) {
 
 const rest = new REST({ version: "10" }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+rest.put(Routes.applicationCommands(clientId), { body: commands })
     .then((data) =>
         console.log(
             `Successfully registered ${data.length} application command(s).`
